b01b3dc20d20cbae00c8a612ee0ea1e1944032ff
[Bini Chand] <[079bct0b33.bini@pcampus.edu.np]>
2024-12-12 22:01:10 +0545

    mongodb connected

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,1 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
+const connectDatabase = async ()=>
+{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,
+            {
+               useNewUrlParser: true,
+               useUnifiedTopology: true 
+            }
+        );
+
+        console.log('MongoDB connected sucessfully');
+    }catch(error)
+    {
+        console.error ('Mongodb connection Failed');
+        process.exit(1);
+    }
+}
+
+module.exports = connectDatabase;
